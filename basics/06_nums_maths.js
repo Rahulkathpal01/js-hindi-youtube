c5d434cd0a84b6c81639d97d25440851cd6b8b8a
Rahul <149292399+Rahulkathpal01@users.noreply.github.com>
2025-07-04 17:51:13 +0000

    date and time

--- basics/06_nums_maths.js
+++ basics/06_nums_maths.js
@@ -3,3 +3,4 @@ const balance = new Number(5000.98);
 console.log(num);
 console.log(balance.toPrecision(5));
 console.log(Math.round(5.6));
+console.log(Math.floor(5488882992929/1000));
---
+++ basics/07_date_time.js
@@ -0,0 +1,5 @@
+let mydate = now.data();
+console.log(mydate.toString()); 
+let myCreatedDate = new Date(2023, 2, 23); // January 1, 2023
+console.log(myCreatedDate.toString());
+mytimeStamp = myCreatedDate.getTime();
