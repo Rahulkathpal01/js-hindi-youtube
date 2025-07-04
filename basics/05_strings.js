0f05f3c26faf033feb1043cbf0a550d580c3f1ff
Rahul <149292399+Rahulkathpal01@users.noreply.github.com>
2025-07-04 14:18:18 +0000

    strings and nums

---
+++ basics/05_strings.js
@@ -0,0 +1,8 @@
+let name = "rahul";
+let repoCount = 50;
+
+// console.log("Hello " + name + ", your repo count is " + repoCount);
+// console.log(`hello my name is ${name} and my repoCount is ${repoCount}`);
+const gamename = new String("Counter Strike");
+console.log(gamename.charAt(0)); 
+console.log(name.charAt(2));
---
+++ basics/06_nums_maths.js
@@ -0,0 +1,5 @@
+const num = 5000
+const balance = new Number(5000.98);
+console.log(num);
+console.log(balance.toPrecision(5));
+console.log(Math.round(5.6));
