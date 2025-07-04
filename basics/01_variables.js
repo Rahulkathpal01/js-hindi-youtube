5cb9cc2e46d36db5a3d1ec181762317896407f2e
Rahul <149292399+Rahulkathpal01@users.noreply.github.com>
2025-07-04 11:58:11 +0000

    02_datatypes

---
+++ basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 4556
+let emailId ="djdjd@gmail.com"
+var AccountPassword = 1234
+accoutCity = "hisar"
+console.log(accountId)
+console.table([accountId , accoutCity , AccountPassword , emailId])
---
+++ basics/02_datatypes.js
@@ -0,0 +1,4 @@
+let name = "Rahul";
+const score = 33;
+var emailid = "rahulkathpal1407@dniddn";
+console.table([name , score, emailid]);
