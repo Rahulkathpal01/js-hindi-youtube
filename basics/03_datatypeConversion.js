a7152cded276b523762c3ceba3c690a5e30bbf20
Rahul <149292399+Rahulkathpal01@users.noreply.github.com>
2025-07-04 12:37:57 +0000

    datatypeConversions

--- basics/03_datatypeConversion.js
+++ basics/03_datatypeConversion.js
@@ -1,6 +1,12 @@
-let score = truenode basics/03_datatypeConversion;
+let score = true;
 let age =96;
 console.log(typeof score);
 let scores = Number(score);
 console.log(scores);
 console.log(typeof scores);
+
+let unge =33;
+let negage =  -unge;
+console.log(unge);
+console.log(negage);
+console.log(2**3);
